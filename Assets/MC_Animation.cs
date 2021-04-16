6ce29e06e1437a2c3750f35f794443f610a64894
julia-koziel <juliakoziel7@gmail.com>
2021-04-16 16:25:38 +0100

    cleaned oldAssets

--- Assets/MC_Animation.cs
+++ Assets/MC_Animation.cs
@@ -8,11 +8,16 @@ public class MC_Animation : MonoBehaviour
 {
     public GameObject Theo;
     public GameObject sabertooth;
+    public GameObject mammoth;
     public GameObject snow;
+    public GameObject shelter;
+    public GameObject left1;
+    public GameObject left;
+    public GameObject right;
     Vector3 camStart = new Vector3(0, 0, -10);
     Vector3 camEnd = new Vector3(0, 0, -10);
-    float camStartSize = 0;
-    float camEndSize = 3;
+    float camStartSize = 5;
+    float camEndSize = 4;
     float camZoomSize = 0.6f;
 
     public GameEvent stageEnd;
@@ -33,20 +38,27 @@ public class MC_Animation : MonoBehaviour
     {
         this.BuildMovement()
             
-            .First(() => {SetActive(sabertooth, Theo);})
-            .Then(stageEnd.Raise)
+            .First(() => {SetActive(sabertooth, Theo, shelter, mammoth);})
+            .Then(duration:5, run: dt => {})
+            
             .Then(() => {
                             cam.orthographicSize = camStartSize;
                             cam.transform.position = camStart;
+
                         })
             .Then(run: time => {
                             cam.orthographicSize = camStartSize + (camEndSize - camStartSize) * Mathf.Pow(time, 0.3f);
                             cam.transform.position = camStart + (camEnd - camStart) * Mathf.Pow(time, 0.3f);
+                            SetActive(snow, right, left); 
+                            SetNonactive(Theo, sabertooth, shelter, left1, mammoth);
+                        
                         }, duration: 2)           
             .Start();
     }
 
     void SetActive(params GameObject[] stimuli) => stimuli.ForEach(s => s.SetActive(true));
+
+    void SetNonactive(params GameObject[] stimuli) => stimuli.ForEach(s => s.SetActive(false));
 
     void Stabilise(params GameObject[] stimuli)
     {
